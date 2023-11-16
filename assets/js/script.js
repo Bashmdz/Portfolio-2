45925e1de256be0109e5237b7f3db21864c473f2
Bashar Zheiman <bashar.zheiman@web.de>
2023-11-16 21:34:14 +0000

    Add more comments in script to explain functions better

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,46 @@
 // Declare constants for scores
-const userScoreElement = document.getElementById("user-score");
-const computerScoreElement = document.getElementById("computer-score");
-const resultElement = document.getElementById("result");
-const explainElement = document.getElementById("explain-game");
+const userScoreElement = document.getElementById("user-score"); /** Reference to the user score element in the DOM */
+const computerScoreElement = document.getElementById("computer-score"); /** Reference to the computer score element in the DOM */
+const resultElement = document.getElementById("result"); /** Reference to the result element in the DOM */
+const explainElement = document.getElementById("explain-game"); /** Reference to the explanation element in the DOM */
 
 // Initialize scores
-let userScore = 0;
-let computerScore = 0;
+let userScore = 0; /** Variable to store the user's score */
+let computerScore = 0; /** Variable to store the computer's score */
 
 // Function to handle user choice
 function userChoice(choice) {
+  /** Check if the game is still ongoing (neither player has reached 3 wins) */
   if (userScore < 3 && computerScore < 3) {
+    /** Define the possible choices in the game */
     const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+    /** Generate a random computer choice */
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
+    /** Get the result of the current round */
     const result = getResult(choice, computerChoice);
 
+    /** Display the result to the user */
     displayResult(result, computerChoice);
   }
 
+  /** Check if either player has won three rounds */
   if (userScore === 3 || computerScore === 3) {
+    /** End the game */
     endGame();
   }
 }
 
 // Function to get the result of the game
 function getResult(user, computer) {
+  /** Check if the user and computer choices are the same (tie) */
   if (user === computer) {
-    resultElement.style.color = "white";
-    return "It's a tie!";
+    resultElement.style.color = "white"; /** Set the result text color to white */
+    return "It's a tie!"; /** Return a tie message */
   }
 
+  /** Check if the user wins based on the rules of the game */
   if (
     (user === "rock" && (computer === "scissors" || computer === "lizard")) ||
     (user === "paper" && (computer === "rock" || computer === "spock")) ||
@@ -38,25 +48,28 @@ function getResult(user, computer) {
     (user === "lizard" && (computer === "spock" || computer === "paper")) ||
     (user === "spock" && (computer === "scissors" || computer === "rock"))
   ) {
-    userScore++;
-    resultElement.style.color = "lightgreen";
-    return "You win!";
+    userScore++; /** Increment the user's score */
+    resultElement.style.color = "lightgreen"; /** Set the result text color to light green */
+    return "You win!"; /** Return a win message */
   } else {
-    computerScore++;
-    resultElement.style.color = "red";
-    return "You lose!";
+    /** The computer wins */
+    computerScore++; /** Increment the computer's score */
+    resultElement.style.color = "red"; /** Set the result text color to red */
+    return "You lose!"; /** Return a lose message */
   }
 }
 
 // Function to display the result
 function displayResult(result, computerChoice) {
+  /** Update the result element with the outcome of the round */
   resultElement.innerHTML = `${result} Computer chose ${computerChoice}.`;
-  userScoreElement.textContent = userScore;
-  computerScoreElement.textContent = computerScore;
+  userScoreElement.textContent = userScore; /** Update the user's score in the DOM */
+  computerScoreElement.textContent = computerScore; /** Update the computer's score in the DOM */
 }
 
 // Function to handle the end of the game
 function endGame() {
+  /** Display a message indicating the winner and restart the game */
   if (userScore === 3) {
     resultElement.innerHTML =
       "Congratulations! You won 3 rounds. Game restarted.";
@@ -64,7 +77,7 @@ function endGame() {
     resultElement.innerHTML = "Computer won 3 rounds! Game restarted.";
   }
 
-  // Reset the scores and display
+  /** Reset the scores and update the display */
   userScore = 0;
   computerScore = 0;
   userScoreElement.textContent = "0";
@@ -73,10 +86,10 @@ function endGame() {
 
 // Function to explain the game
 function explainGame() {
-  // Toggle the visibility of the explanation element
+  /** Toggle the visibility of the explanation element */
   explainElement.style.display = explainElement.style.display === "none" ? "block" : "none";
 
-  // If the explanation is visible, update its content
+  /** If the explanation is visible, update its content */
   if (explainElement.style.display !== "none") {
     explainElement.innerHTML =
       "Welcome to Rock Paper Scissors Lizard Spock!<br><br>" +
@@ -97,11 +110,11 @@ function explainGame() {
 
 // Event listeners for buttons
 document.querySelectorAll(".btn").forEach((button) => {
+  /** Add a click event listener to each game button */
   button.addEventListener("click", () => {
     userChoice(button.dataset.choice);
   });
 });
 
+// Event listener for the explain-button
 document.getElementById("explain-button").addEventListener("click", explainGame);
-
-document.addEventListener("click", () => document.getElementById("iframeAudio").play());
